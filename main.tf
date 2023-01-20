ff29473c5800f412c5c0fd677254dc468db453cc
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2023-01-20 08:58:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,22 @@ module "cloudrun" {
   version = "0.0.2"
 }
   
-resource "random_pet" "name2" {
+resource "random_pet" "name222" {
  length    = "15"
  separator = "-"
 }
 
-resource "random_pet" "name3" {
+resource "random_pet" "name333" {
  length    = "8"
  separator = "-"
+}
+
+  resource "null_resource" "georgiman111" {
+  triggers {
+    build_number = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo hello georgiman"
+  }
 }
