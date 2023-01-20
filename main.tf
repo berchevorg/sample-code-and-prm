52195b90a6a110998aa7d2af5e9fd001381c8b2f
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2023-01-20 08:40:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-module "cloudrun" {
-  source  = "app.terraform.io/georgiman-tfc4b/cloudrun/google"
-  version = "0.0.2"
-}
+# module "cloudrun" {
+#   source  = "app.terraform.io/georgiman-tfc4b/cloudrun/google"
+#   version = "0.0.2"
+# }
   
 resource "random_pet" "name2" {
- length    = "11"
+ length    = "12"
  separator = "-"
 }
 
 resource "random_pet" "name3" {
- length    = "4"
+ length    = "5"
  separator = "-"
 }
